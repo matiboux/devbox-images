28093b4136ae8cd9b85fe84dfc7451b4112c36b5
Matiboux <matiboux@gmail.com>
2026-07-20 04:01:55 +0200

    Ignore root user action in Python tools install

--- src/common/install-python-tools.sh
+++ src/common/install-python-tools.sh
@@ -4,4 +4,7 @@ set -e
 PYTHON_COMMAND="$(command -v python3 || command -v python)"
 
 # Install Python development tools
-${PYTHON_COMMAND} -m pip install --no-cache-dir ruff pytest
+${PYTHON_COMMAND} -m pip install \
+	--no-cache-dir \
+	--root-user-action ignore \
+	ruff pytest
