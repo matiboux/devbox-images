6e1caf3c4c5c15eca4416aea47d74fc034183d2e
Mati <matiboux@gmail.com>
2026-07-22 19:04:05 +0200

    Update & Use image tag generator (#11)

---
+++ scripts/image_tag.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+from itertools import product
+from typing import List, Sequence, Tuple
+
+
+class ImageTagGenerator:
+
+    def __init__(
+        self,
+        components: Sequence[Tuple[str, str] | Tuple[str, str, str]],
+    ):
+        """
+        Initialize the ImageTagGenerator with component versions and tag levels.
+        :param components: List of tuples containing (component_name, version, tag_level)
+        """
+        self.components: List[Tuple[str, str, str]] = [
+            (
+                (comp[0], comp[1], self._validate_tag_level(comp[2]))
+                if len(comp) >= 3 else
+                (comp[0], comp[1], 'patch')
+            )
+            for comp in components
+        ]
+
+        self.image_tags: List[str] = []
+
+    @staticmethod
+    def _validate_tag_level(level: str) -> str:
+        if level in ('global', 'major', 'minor', 'patch'):
+            return level
+        return 'patch'
+
+    def _get_component_options(self, version: str, level: str) -> List[str]:
+
+        if not version:
+            return ['']
+
+        version_parts = version.split('.')
+        major = version_parts[0]
+        minor = f"{version_parts[0]}.{version_parts[1]}" if len(version_parts) >= 2 else version
+
+        raw_options: List[str] = []
+        if level == 'global':
+            raw_options = [version, minor, major, '']
+        elif level == 'major':
+            raw_options = [version, minor, major]
+        elif level == 'minor':
+            raw_options = [version, minor]
+        else:  # 'patch'
+            raw_options = [version]
+
+        # De-duplicate while preserving order
+        options: List[str] = []
+        for item in raw_options:
+            if item not in options:
+                options.append(item)
+
+        return options
+
+    def _get_prefixed_options(self, prefix: str, version: str, tag_level: str) -> List[str]:
+
+        if not version:
+            return ['']
+
+        options = self._get_component_options(version, tag_level)
+        return [f"{prefix}{opt}" for opt in options]
+
+    def generate_tags(
+        self,
+        only_fully_qualified: bool = False,
+    ) -> List[str]:
+
+        component_options_list = []
+        tag_level_override = 'patch' if only_fully_qualified else None
+        for index, (comp_name, comp_version, comp_tag_level) in enumerate(self.components):
+            if index == 0:
+                options = self._get_component_options(comp_version, tag_level_override or comp_tag_level)
+            else:
+                options = self._get_prefixed_options(comp_name, comp_version, tag_level_override or comp_tag_level)
+            component_options_list.append(options)
+
+        tags: List[str] = []
+        for component_values in product(*component_options_list):
+            tag_pieces: List[str] = []
+
+            for i, (comp_name, _, _) in enumerate(self.components):
+                if component_values[i]:
+                    tag_pieces.append(component_values[i])
+
+            if not tag_pieces:
+                image_tag = 'latest'
+            else:
+                image_tag = '-'.join(tag_pieces)
+
+            tags.append(image_tag)
+
+        self.image_tags = tags
+        return tags
+
+    def print_tags(
+        self,
+        compact_output: bool = False,
+    ) -> None:
+        """
+        Print the generated image tags to stdout.
+        :param compact_output: If True, output tags as comma-separated values on a single line
+        """
+        if compact_output:
+            print(','.join(self.image_tags))
+        else:
+            for tag in self.image_tags:
+                print(tag)
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description='Generate Docker image tags for the devbox image.',
+    )
+    parser.add_argument(
+        'components',
+        nargs='+',
+        help='List of components in the format: component_name=version[:tag_level]. Example: python=3.14.6:global =slim poetry=20.5.1:minor',
+    )
+    parser.add_argument(
+        '-c', '--compact',
+        action='store_true',
+        help='Output tags as comma-separated values on a single line',
+    )
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+
+    components: List[Tuple[str, str] | Tuple[str, str, str]] = []
+    for comp in args.components:
+        if '=' not in comp:
+            print(f"Invalid component format: {comp}. Expected format: component_name=version[:tag_level]", file=sys.stderr)
+            sys.exit(1)
+        comp_name, version_tag = comp.split('=', 1)
+        if ':' in version_tag:
+            version, tag_level = version_tag.split(':', 1)
+            components.append((comp_name, version, tag_level))
+        else:
+            components.append((comp_name, version_tag))
+
+    generator = ImageTagGenerator(
+        components=components,
+    )
+
+    generator.generate_tags()
+
+    generator.print_tags(
+        compact_output=args.compact,
+    )
+
+
+if __name__ == "__main__":
+    main()
