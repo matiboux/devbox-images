a2e6d43c11bb78f097ab752743501368be513179
Matiboux <matiboux@gmail.com>
2026-07-25 03:20:49 +0200

    Fix version tags prefix for Yarn

--- src/common/install-nvm.sh
+++ src/common/install-nvm.sh
@@ -31,6 +31,7 @@ fi
 get_nvm_version() {
 	local version="$1"
 	local github_repo='nvm-sh/nvm'
+	local version_prefix='v'
 	local version_full="$(echo "${version}" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' || true)"
 	if [ -n "${version_full}" ]; then
 		echo "${version_full}"
@@ -64,7 +65,7 @@ get_nvm_version() {
 			| sed 's/^v//'
 		)
 	else
-		response=$(curl -sSL -w "\n%{http_code}" "https://api.github.com/repos/${github_repo}/git/matching-refs/tags/v${version}") || {
+		response=$(curl -sSL -w "\n%{http_code}" "https://api.github.com/repos/${github_repo}/git/matching-refs/tags/${version_prefix}${version}") || {
 			echo 'Failed to connect to GitHub API.' >&2
 			return 1
 		}
@@ -87,7 +88,7 @@ get_nvm_version() {
 		version_full=$(
 			echo "${response}" \
 			| sed -n 's/.*"ref"[ ]*:[ ]*"\([^"]*\)".*/\1/p' \
-			| sed 's|refs/tags/v||' \
+			| sed "s|refs/tags/${version_prefix}||" \
 			| sort -V \
 			| tail -n1
 		)
