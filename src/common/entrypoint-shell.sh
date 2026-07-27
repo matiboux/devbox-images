e75e0625f46cb8be0431ecaa85fba4a3e412c551
Mathieu Guérin <mathieu.guerin@saegus.com>
2026-07-27 19:04:41 +0000

    Fix shell in entrypoint scripts

--- src/common/entrypoint-shell.sh
+++ src/common/entrypoint-shell.sh
@@ -5,8 +5,12 @@ if [ -s "${NVM_DIR}/nvm.sh" ]; then
     \. "${NVM_DIR}/nvm.sh"
 fi
 
+if [ -z "${SHELL}" ]; then
+	SHELL="$(command -v bash || command -v sh)"
+fi
+
 if [ $# -gt 0 ] && [ "$1" = "${1#-}" ]; then
 	exec "$@"
 else
-	exec "${SHELL:-/bin/sh}" "$@"
+	exec "${SHELL}" "$@"
 fi
