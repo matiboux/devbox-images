d7bd5329b5e90caffac508cb20a091e0bce0fc2b
Matiboux <matiboux@gmail.com>
2026-08-02 03:51:35 +0200

    Install mypy in python tools

--- src/common/install-python-tools.sh
+++ src/common/install-python-tools.sh
@@ -7,4 +7,6 @@ PYTHON_COMMAND="$(command -v python3 || command -v python)"
 ${PYTHON_COMMAND} -m pip install \
 	--no-cache-dir \
 	--root-user-action ignore \
-	ruff pytest
+	mypy \
+	pytest \
+	ruff
